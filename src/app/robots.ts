c937bf7a26b49b5b2c4d6f0eb075f4fa6ab38d4a
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2026-07-17 15:47:11 +0530

    update domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://bakers-hut.example/sitemap.xml",
+    sitemap: "https://bakershutcake.com/sitemap.xml",
   };
 }
